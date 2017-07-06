04023090324d7e9e8db027ecf8de0d92ac47c67d
Ayke van Laethem <aykevanlaethem@gmail.com>
2017-07-06 16:49:57 +0200

    Add methods from BIGConfiguration

--- src/main/java/foundation/privacybydesign/common/BaseConfiguration.java
+++ src/main/java/foundation/privacybydesign/common/BaseConfiguration.java
@@ -3,12 +3,10 @@ package foundation.privacybydesign.common;
 import java.io.*;
 import java.net.URL;
 import java.net.URLConnection;
-import java.security.KeyFactory;
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
-import java.security.PrivateKey;
+import java.security.*;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 
 /**
  * Created by ayke on 19-6-17.
@@ -71,6 +69,27 @@ public class BaseConfiguration {
             PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(rawKey);
             return KeyFactory.getInstance("RSA").generatePrivate(spec);
         } catch (NoSuchAlgorithmException |InvalidKeySpecException e) {
+            throw new KeyManagementException(e);
+        }
+    }
+
+    public PublicKey loadPublicKey(String filename) throws
+            KeyManagementException {
+        try {
+            return decodePublicKey(BaseConfiguration.getResource(filename));
+        } catch (IOException e) {
+            throw new KeyManagementException(e);
+        }
+    }
+
+    private PublicKey decodePublicKey(byte[] rawKey) throws KeyManagementException {
+        try {
+            if (rawKey == null || rawKey.length == 0)
+                throw new KeyManagementException("Could not read public key");
+
+            X509EncodedKeySpec spec = new X509EncodedKeySpec(rawKey);
+            return KeyFactory.getInstance("RSA").generatePublic(spec);
+        } catch (NoSuchAlgorithmException|InvalidKeySpecException e) {
             throw new KeyManagementException(e);
         }
     }
